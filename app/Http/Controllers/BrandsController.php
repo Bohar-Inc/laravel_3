c9c99a73088e74981d19ba69dbf82d36049bb038
PURUS01 <purus5718@gmail.com>
2025-01-03 16:26:32 +0530

    create brand model, migration and resource controller and create crud operation to brand

---
+++ app/Http/Controllers/BrandsController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+
+class BrandsController extends Controller
+{
+    use ValidatesRequests;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $brands = Brand::orderby('created_at', 'desc')->get();
+        return view('brands.index',compact('brands'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('brands.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:2|max:50|unique:brands',
+        ]);
+
+        $brand=new Brand();
+        $brand->name=$request->name;
+        $brand->save();
+        \flash('Brand created successfully!')->success();
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $brand=Brand::findOrFail($id);
+        return view('brands.edit',compact('brand'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,$id)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:2|max:50|unique:brands,name,'.$id,
+        ]);
+
+        $brand=Brand::findOrFail($id);
+        $brand->name=$request->name;
+        $brand->save();
+        \flash('Brand updated successfully!')->success();
+        return redirect()->route('brands.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $brand=Brand::findOrFail($id);
+        $brand->delete();
+        \flash('Brand deleted successfully!')->success();
+        return redirect()->route('brands.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,7 +20,10 @@ Route::middleware([
 
 Route::middleware(['auth:sanctum'])->group(function (){
     //Category
-    Route::resource('categories',\App\Http\Controllers\CategoriesController::class );
+    Route::resource('categories',CategoriesController::class );
+
+    //Brand
+    Route::resource('brands',BrandsController::class);
 
 });
 
