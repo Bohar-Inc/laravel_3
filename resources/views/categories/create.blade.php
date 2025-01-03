2a308652ce6830664402cbf9648a05f151ecaad3
PURUS01 <purus5718@gmail.com>
2025-01-03 14:54:49 +0530

    Create categories and compact to the views and  add datatables link ,scripts

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -26,36 +26,25 @@
 
                     <div class="card card-primary card-outline">
                         <div class="card-body">
-                            <h5 class="card-title">Create Category</h5>
+                            <h5 class="card-title">Create Category</h5><br>
 
                             <!-- form start -->
-                            <form class="form-horizontal">
+                            <form role="form" action="{{route('categories.store')}}" method="post">
+                                @csrf
                                 <div class="card-body">
-                                    <div class="form-group row">
-                                        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                                        <div class="col-sm-10">
-                                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter category name">
+                                        @if($errors->has('name'))
+                                            <span class="text-danger">{{$errors->first('name')}}</span>
+
+                                        @endif
                                     </div>
-                                    <div class="form-group row">
-                                        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                                        <div class="col-sm-10">
-                                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <div class="offset-sm-2 col-sm-10">
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                                                <label class="form-check-label" for="exampleCheck2">Remember me</label>
-                                            </div>
-                                        </div>
-                                    </div>
+
                                 </div>
                                 <!-- /.card-body -->
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-info">Sign in</button>
-                                    <button type="submit" class="btn btn-default float-right">Cancel</button>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                                 <!-- /.card-footer -->
                             </form>
--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -11,4 +11,7 @@
 <!-- Google Font: Source Sans Pro -->
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('admin/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
 @stack('css')
