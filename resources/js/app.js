cac94e5b357f85b84c94da2915dcf29563f9b516
PURUS01 <purus5718@gmail.com>
2025-01-04 15:54:22 +0530

    Installed vue and setup the vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue'; // Import Vue
+import ExampleComponent from '@/components/ExampleComponents.vue'; // Import Vue component
+
+const app = createApp({}); // Create Vue app instance
+app.component('ExampleComponent', ExampleComponent); // Register Vue component
+app.mount('#app'); // Mount the app to a DOM element with id="app"
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,17 +6,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <html lang="en">
 <head>
 @include('layouts.partials._head')
+    @vite('resources/js/app.js')
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 @include('layouts.partials._navbar')
 @include('layouts.partials._sidebar')
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <div class="content-wrapper" >
         @include('flash::message')
         <!-- Content Header (Page header) -->
         @yield('content')
+
     </div>
     <!-- /.content-wrapper -->
 @include('layouts.partials._footer')
